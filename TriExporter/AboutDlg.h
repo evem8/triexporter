796ca0da2718c8da9f4ae5eec4fdadcfbfeb0949
SND\Selvin_cp <SND\Selvin_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2012-08-08 10:03:12 +0000

    AboutBox changes ... time to realease the project
    git-svn-id: https://triexporter.svn.codeplex.com/svn@21257 527b9aa4-23f3-4c7d-acba-bb8337ca51bc

--- TriExporter/AboutDlg.h
+++ TriExporter/AboutDlg.h
@@ -15,6 +15,8 @@ public:
 		COMMAND_ID_HANDLER(IDCANCEL, OnCloseCmd)
 	END_MSG_MAP()
 
+	CEdit m_FBX;
+
 // Handler prototypes (uncomment arguments if needed):
 //	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 //	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
@@ -23,6 +25,8 @@ public:
 	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 	{
 		CenterWindow(GetParent());
+		m_FBX.Attach(GetDlgItem(IDC_EDITFBX));
+		m_FBX.SetWindowTextA("This software contains Autodesk® FBX® code developed by Autodesk, Inc. Copyright 2012 Autodesk, Inc. All rights, reserved. Such code is provided “as is” and Autodesk, Inc. disclaims any and all warranties, whether express or implied, including without limitation the implied warranties of merchantability, fitness for a particular purpose or non-infringement of third party rights. In no event shall Autodesk, Inc. be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of such code");
 		return TRUE;
 	}
 
